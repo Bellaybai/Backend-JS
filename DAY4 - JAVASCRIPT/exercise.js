711d1757d96b60e98f5c74def1a1e5808dc42ba9
Bella <yunbai226@gmail.com>
2024-02-26 21:51:18 -0800

    Day 4 JS

---
+++ DAY4 - JAVASCRIPT/exercise.js
@@ -0,0 +1,13 @@
+function checkEvenOrOdd(num){
+    if(num%2 == 0){
+        console.log(num, "is even.");
+    }else{
+        console.log(num, "is odd.");
+    }
+
+}
+const num1 = 10;
+const num2 = 7;
+
+checkEvenOrOdd(num1);
+checkEvenOrOdd(num2);
